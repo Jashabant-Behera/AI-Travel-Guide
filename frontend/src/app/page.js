b66c4ddcdba700ac49cd61a7343c2fb78a72c503
Jashabant Behera <jeetbehera143@gmail.com>
2025-04-11 12:15:37 +0530

    cmt

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -9,17 +9,11 @@ import Docs from "../components/Docs";
 import Footer from "../components/Footer";
 import Testimonial from "../components/Testimonial";
 import Faq from "../components/faq";
-import Chatbot from "@/components/ChatBot";
-import ItineraryCard from "@/components/ItineraryCard";
-import Recommendations from "@/components/Recommendations";
-import ResetPassword from "@/components/ResetPassword";
-import SavedItineraries from "@/components/SavedItineraries";
-import VerifyEmail from "@/components/VerifyEmail";
 
 export default function Home() {
   return (
     <main>
-      {/* <Navbar />
+      <Navbar />
       <Header />
       <Features />
       <Details />
@@ -28,12 +22,6 @@ export default function Home() {
       <Faq />
       <About />
       <Footer />
-      <SavedItineraries />
-      <VerifyEmail/>
-      <Chatbot />
-      <ItineraryCard />
-      <Recommendations /> */}
-      <ResetPassword/>
     </main>
   );
 }
